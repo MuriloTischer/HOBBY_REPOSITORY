20cc32c60fb57b99518a278cee7f9dc59b3abeb8
MuriloTischer <muutischer@outlook.com>
2024-06-30 18:59:42 -0300

    first commit

---
+++ dart_flutter/dart_programming_logic/bin/dart_programming_logic.dart
@@ -0,0 +1,5 @@
+import 'package:dart_programming_logic/dart_programming_logic.dart' as dart_programming_logic;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_programming_logic.calculate()}!');
+}
---
+++ dart_flutter/dart_programming_logic/test/dart_programming_logic_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_programming_logic/dart_programming_logic.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
