a38e534c504eaac65b08fbf04b52e108a0ef8f57
MuriloTischer <muutischer@outlook.com>
2024-06-30 19:05:15 -0300

    first commit

--- dart_flutter/dart_programming_logic/bin/dart_programming_logic.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_programming_logic/dart_programming_logic.dart' as dart_programming_logic;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_programming_logic.calculate()}!');
-}
